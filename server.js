eafe2552f84d62a6618a417ee79c486cd768cb6b
paresh05 <pareshpraveen.surathkal@gmail.com>
2021-10-07 19:56:16 +0530

    Added: Easy Notes App

---
+++ server.js
@@ -0,0 +1,31 @@
+const dbConfig = require("./config/database.config.js");
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
+app.use(express.json());
+require("./app/routes/note.routes.js")(app);
+
+// listen for requests
+app.listen(3000, () => {
+  console.log("Server is listening on port 3000");
+});
+
+// Connecting to the database
+mongoose
+  .connect(dbConfig.url, {
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log("Successfully connected to the database");
+  })
+  .catch((err) => {
+    console.log("Could not connect to the database. Exiting now...", err);
+    process.exit();
+  });
